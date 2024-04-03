8aab8dfe7611cc8f37a1424dc36942706db1e5f8
Eshal Akram <halenoor.20@gmail.com>
2024-04-03 14:35:17 -0700

    2nd commit

---
+++ index.ts
@@ -0,0 +1,48 @@
+//Declare a variable for storing your favorite movie name with a string type annotation.
+let movie_name:string="The Childe";
+console.log(movie_name);
+//Create a template string to introduce yourself, including your name and age.
+let my_firstname:string="Eshal";
+let my_lastname:string="Akram";
+let my_age:number=22;
+let my_introduction=`Assalam-o-Alaikum:) i'm ${my_firstname}${my_lastname} i'm ${my_age} years old & I wanna become Developer`;
+console.log(my_introduction);
+//Initialize a constant with a type annotation for your current mood as a boolean.
+const my_currentmood:boolean=true;
+console.log(my_currentmood);
+//Combine a user's first and last name using template literals and log the full name.
+let user_firstname=my_firstname;
+let user_lastname=my_lastname;
+let user_fullname=`${user_firstname}${user_lastname}`;
+console.log(user_fullname);
+//Create a variable for a user's age with a type annotation and assign a number to it.
+let user_age:number=22;
+console.log(user_age);
+//Use a template string to display a product name and its price.
+let product_name="Sunscreen";
+let product_price=2000;
+console.log`${product_name}${product_price}`;
+//Declare a variable to store the number of pets you have with a number type annotation.
+let my_pets:number=0;
+console.log(my_pets);
+//Declare a variable with a boolean type annotation indicating if it is your birthday today.
+let is_mybirthday:boolean=false;
+console.log(is_mybirthday);
+//Create a template string that includes a user's first name, last name, and age with proper labels.
+let user_details=`${user_firstname}${user_lastname} ${user_age}`;
+console.log(user_details);
+//Initialize a constant to hold your birth year with a number type annotation.
+const my_birthyear:number=2001;
+console.log(my_birthyear);
+//Write a line of code that uses a template string to combine a city and country into a full address.
+let my_country:string="Pakistan";
+let my_city:string="Lahore";
+let my_fulladdress=`${my_city} ${my_country}`;
+console.log(my_fulladdress);
+//Declare a variable for today's temperature with a number type annotation and assign a value to it.
+let today_temperature:Number=18;
+console.log(today_temperature);
+
+
+
+
